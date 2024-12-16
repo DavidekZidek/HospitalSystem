c930743a66b48d16c3e0c9ec5d99aad0149f484b
Martin Berčík <m1_bercik@utb.cz>
2024-12-16 12:37:44 +0100

    migrace

---
+++ HospitalSystem/HospitalSystem.Infrastructure/Migrations/20241216110957_InitialMigration.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "UserAccounts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2024, 12, 16, 12, 9, 57, 685, DateTimeKind.Local).AddTicks(5800));
+
+            migrationBuilder.UpdateData(
+                table: "UserAccounts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2024, 12, 16, 12, 9, 57, 685, DateTimeKind.Local).AddTicks(5840));
+
+            migrationBuilder.UpdateData(
+                table: "UserAccounts",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2024, 12, 16, 12, 9, 57, 685, DateTimeKind.Local).AddTicks(5840));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "UserAccounts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2024, 12, 16, 12, 8, 35, 367, DateTimeKind.Local).AddTicks(2400));
+
+            migrationBuilder.UpdateData(
+                table: "UserAccounts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2024, 12, 16, 12, 8, 35, 367, DateTimeKind.Local).AddTicks(2420));
+
+            migrationBuilder.UpdateData(
+                table: "UserAccounts",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2024, 12, 16, 12, 8, 35, 367, DateTimeKind.Local).AddTicks(2420));
+        }
+    }
+}
