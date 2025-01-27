43d6fba6a10d8ae6508b4abfee19012d67a25313
Martin Berčík <m1_bercik@utb.cz>
2025-01-27 20:41:01 +0100

    usera

--- HospitalSystem/HospitalSystem.Infrastructure/Seeding/UserAccountInit.cs
+++ HospitalSystem/HospitalSystem.Infrastructure/Seeding/UserAccountInit.cs
@@ -10,7 +10,11 @@ public static class UserAccountInit
         {
             new UserAccount { Id = 1, Name = "Admin", Password = "Admin123", CreatedAt = DateTime.Now, RoleId = 1 },
             new UserAccount { Id = 2, Name = "DoctorJohn", Password = "Doctor123", CreatedAt = DateTime.Now, RoleId = 2 },
-            new UserAccount { Id = 3, Name = "PatientJane", Password = "Patient123", CreatedAt = DateTime.Now, RoleId = 3 }
+            new UserAccount { Id = 3, Name = "PatientJane", Password = "Patient123", CreatedAt = DateTime.Now, RoleId = 3 },
+            new UserAccount { Id = 4, Name = "PatientJane", Password = "Patient123", CreatedAt = DateTime.Now, RoleId = 3 },
+            new UserAccount { Id = 5, Name = "PatientJane", Password = "Patient123", CreatedAt = DateTime.Now, RoleId = 3 },
+            new UserAccount { Id = 6, Name = "PatientJane", Password = "Patient123", CreatedAt = DateTime.Now, RoleId = 3 },
+            new UserAccount { Id = 7, Name = "PatientJane", Password = "Patient123", CreatedAt = DateTime.Now, RoleId = 3 }
         };
     }
 }
